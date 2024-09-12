52c4819b666445fe697a3c825ac0ba8248d8c0a5
Musaab Abdalla <jamboosan209@gmail.com>
2024-09-12 20:59:17 +0200

    adds and configures the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SiGit } from "react-icons/si";
+import logo from "./assets/logo-no-background.png";
+import { FiMenu } from "react-icons/fi";
+import { RiCloseLargeFill } from "react-icons/ri";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Nav() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <nav className="fixed left-0 right-0 top-0 flex w-full items-center justify-between p-4">
+      <div className="flex items-center gap-12">
+        <SiGit className="text-4xl" />
+        <div className="flex items-center gap-6">
+          <a href="#">Product</a>
+          <a href="#">Solution</a>
+          <a href="#">Blog</a>
+          <a href="#">Pricing</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <button className="rounded-md bg-indigo-500 px-3 py-2 text-sm font-medium text-white">
+        Join Newsletter
+      </button>
+    </nav>
+  );
 }
 
-export default App
+function Nava() {
+  return (
+    <header className="bg-white">
+      <nav className="mx-auto flex w-[92%] items-center justify-between pt-3">
+        <div>
+          <img className="w-35 h-16" src={logo} alt="Holidaze logo" />
+        </div>
+        <div className="nav-links absolute left-0 top-[-100%] flex min-h-[60vh] w-full items-center bg-white px-5 md:static md:min-h-fit md:w-auto">
+          <ul className="flex flex-col gap-8 md:flex-row md:items-center md:gap-[4vw]">
+            <li>
+              <a className="hover:text-gray-500" href="#">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-gray-500" href="#">
+                Contact us
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-gray-500" href="#">
+                About us
+              </a>
+            </li>
+            <li>
+              <a className="hover:text-gray-500" href="#">
+                Profile
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center gap-6">
+          <button className="rounded-full bg-[#a6c1ee] px-5 py-2 text-white hover:bg-[#87acec]">
+            Sign in
+          </button>
+          <div
+            onClick={onToggleMenu}
+            className="open-icon cursor-pointer text-3xl md:hidden"
+          >
+            <FiMenu />
+          </div>
+          <div
+            onClick={onToggleMenu}
+            className="close-icon hidden cursor-pointer text-3xl md:hidden"
+          >
+            <RiCloseLargeFill />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+function onToggleMenu() {
+  const navLinks = document.querySelector(".nav-links");
+  const close = document.querySelector(".close-icon");
+  const open = document.querySelector(".open-icon");
+  close.classList.toggle("hidden");
+  open.classList.toggle("hidden");
+  navLinks.classList.toggle("top-[-100%]");
+  navLinks.classList.toggle("top-[18%]");
+  console.log(navLinks);
+}
+
+function App() {
+  return (
+    <>
+      <Nava />
+    </>
+  );
+}
+
+export default App;
