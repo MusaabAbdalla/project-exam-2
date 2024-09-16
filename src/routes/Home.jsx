089bc8e5042c6e5c012740c2ed392fb72fdc8f76
Musaab Abdalla <jamboosan209@gmail.com>
2024-09-16 12:05:58 +0200

    working wiht Home-page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import venue from "../assets/venue.jpg";
+import { Button } from "@/components/ui/button";
 
 const API = "https://v2.api.noroff.dev/holidaze/venues";
 
@@ -18,7 +20,26 @@ function Home() {
 
   return (
     <div className="p-96">
-      <h1 className="font-bold">This is Home</h1>
+      <div>
+        <div className="w-1/3 rounded-md border bg-gray-50">
+          <div>
+            <img src={venue} alt="not available" />
+          </div>
+          <div className="flex items-start justify-between">
+            <div className="flex flex-col items-start">
+              <p className="text-primary-90 text-xl">Beach apartment</p>
+              <p className="text-lg">US$ 120, - per night</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <p className="text-lg">Very Good</p>
+              <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-lg text-white transition-shadow hover:rounded-lg">
+                8.9
+              </p>
+            </div>
+          </div>
+          <Button className="bg-secondary-100">Check Availability</Button>
+        </div>
+      </div>
     </div>
   );
 }
