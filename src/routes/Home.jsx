2de408503b4fc52802e6d5e9babe394425b3640c
Musaab Abdalla <jamboosan209@gmail.com>
2024-09-17 08:50:18 +0200

    error showing venues in home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,30 +15,33 @@ function Home() {
     }
     getData();
   }, []);
+  const venuesArray = venues?.data;
 
-  console.log(venues);
+  console.log(venuesArray);
 
   return (
     <div className="p-96">
       <div>
-        <div className="w-1/3 rounded-md border bg-gray-50">
-          <div>
-            <img src={venue} alt="not available" />
-          </div>
-          <div className="flex items-start justify-between">
-            <div className="flex flex-col items-start">
-              <p className="text-primary-90 text-xl">Beach apartment</p>
-              <p className="text-lg">US$ 120, - per night</p>
+        {venuesArray.map((venue) => (
+          <div key={venue.id} className="rounded-md border bg-gray-50 md:w-1/3">
+            <div>
+              <img className="w-full" src={venue} alt="not available" />
             </div>
-            <div className="flex items-center gap-1">
-              <p className="text-lg">Very Good</p>
-              <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-lg text-white transition-shadow hover:rounded-lg">
-                8.9
-              </p>
+            <div className="flex items-start justify-between">
+              <div className="flex flex-col items-start">
+                <p className="text-primary-90 text-xl">Beach apartment</p>
+                <p className="text-lg">US$ 120, - per night</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-lg">Very Good</p>
+                <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-lg text-white transition-shadow hover:rounded-lg">
+                  8.9
+                </p>
+              </div>
             </div>
+            <Button className="bg-secondary-100">Check Availability</Button>
           </div>
-          <Button className="bg-secondary-100">Check Availability</Button>
-        </div>
+        ))}
       </div>
     </div>
   );
