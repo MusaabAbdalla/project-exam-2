804175f2a6a4cdd99c46701fddf3c48f45e96867
Musaab Abdalla <musabd50118@stud.noroff.no>
2024-09-17 20:26:18 +0200

    fixes problem with fetching data from api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import venue from "../assets/venue.jpg";
+import ven from "../assets/venue.jpg";
 import { Button } from "@/components/ui/button";
 
 const API = "https://v2.api.noroff.dev/holidaze/venues";
@@ -21,27 +21,31 @@ function Home() {
 
   return (
     <div className="p-96">
-      <div>
-        {venuesArray.map((venue) => (
-          <div key={venue.id} className="rounded-md border bg-gray-50 md:w-1/3">
-            <div>
-              <img className="w-full" src={venue} alt="not available" />
-            </div>
-            <div className="flex items-start justify-between">
-              <div className="flex flex-col items-start">
-                <p className="text-primary-90 text-xl">Beach apartment</p>
-                <p className="text-lg">US$ 120, - per night</p>
+      <div className=" flex">
+        {venuesArray ? (
+          venuesArray.map((venue) => (
+            <div key={venue.id} className="rounded-md border bg-gray-50 md:w-1/3">
+              <div>
+                <img className="w-full" src={ven} alt="not available" />
               </div>
-              <div className="flex items-center gap-1">
-                <p className="text-lg">Very Good</p>
-                <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-lg text-white transition-shadow hover:rounded-lg">
-                  8.9
-                </p>
+              <div className="flex items-start justify-between">
+                <div className="flex flex-col items-start">
+                  <p className="text-primary-90 text-xl">Beach apartment</p>
+                  <p className="text-lg">{venue.price} per night</p>
+                </div>
+                <div className="flex items-center gap-1">
+                  <p className="text-lg">very good</p>
+                  <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-lg text-white transition-shadow hover:rounded-lg">
+                   8.9 
+                  </p>
+                </div>
               </div>
+              <Button className="bg-secondary-100">Check Availability</Button>
             </div>
-            <Button className="bg-secondary-100">Check Availability</Button>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>Loading venues...</p> // Show a loading state while data is being fetched
+        )}
       </div>
     </div>
   );
