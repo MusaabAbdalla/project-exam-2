9cc50163fb2f91a085c92795716006932d8cb556
Musaab Abdalla <jamboosan209@gmail.com>
2024-09-18 08:45:07 +0200

    finishes basic styling for home-page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -20,27 +20,33 @@ function Home() {
   console.log(venuesArray);
 
   return (
-    <div className="p-96">
-      <div className=" flex">
+    <div className="mx-auto max-w-screen-2xl px-16">
+      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
         {venuesArray ? (
           venuesArray.map((venue) => (
-            <div key={venue.id} className="rounded-md border bg-gray-50 md:w-1/3">
-              <div>
-                <img className="w-full" src={ven} alt="not available" />
+            <div key={venue.id} className="rounded-md border bg-gray-50">
+              <div className="aspect-h-1 aspect-w-1 lg:aspect-none w-full overflow-hidden group-hover:opacity-75 lg:h-80">
+                <img
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  src={ven}
+                  alt="not available"
+                />
               </div>
-              <div className="flex items-start justify-between">
+              <div className="mt-2 flex items-start justify-between px-4">
                 <div className="flex flex-col items-start">
                   <p className="text-primary-90 text-xl">Beach apartment</p>
                   <p className="text-lg">{venue.price} per night</p>
                 </div>
                 <div className="flex items-center gap-1">
-                  <p className="text-lg">very good</p>
-                  <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-lg text-white transition-shadow hover:rounded-lg">
-                   8.9 
+                  <p className="text-sm">very good</p>
+                  <p className="bg-primary-90 rounded-t-lg rounded-br-lg p-2 text-xs text-white transition-shadow hover:rounded-lg">
+                    8.9
                   </p>
                 </div>
               </div>
-              <Button className="bg-secondary-100">Check Availability</Button>
+              <div className="mt-8 flex justify-end p-2">
+                <Button className="bg-secondary-100">Check Availability</Button>
+              </div>
             </div>
           ))
         ) : (
