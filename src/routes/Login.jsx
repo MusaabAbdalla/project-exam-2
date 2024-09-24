ab7c08ea2da548675ea3b1373111b2606ba0ff38
Musaab Abdalla <jamboosan209@gmail.com>
2024-09-24 09:49:18 +0200

    finishes login page with error messages

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import {
   Card,
   CardContent,
@@ -8,56 +9,106 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { userRegister } from "@/api/auth/register";
+import { userLogin } from "@/api/auth/login";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const userName = "jamboo";
-const userEmail = "jamboo@stud.noroff.no";
-const userPassword = "as123456";
-// userRegister(userName, userEmail, userPassword);
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("email should be valid email")
+      .required("email is required"),
+    password: yup.string().min(8, "Password must be at least 8 characters"),
+  })
+  .required();
 
 export const description =
   "A login form with email and password. There's an option to login with Google and a link to sign up if you don't have an account.";
 
 export default function LoginForm() {
+  const navigate = useNavigate();
+  const [loginError, setLoginError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  async function onSubmit(data) {
+    const { email, password } = data;
+    try {
+      const result = await userLogin(email, password);
+      if (result) {
+        console.log("everything is fine");
+        navigate("/profile");
+      } else {
+        setLoginError("Invalid Username of Password");
+        setTimeout(() => {
+          setLoginError("");
+        }, 2000);
+        console.log("Login Failed");
+      }
+    } catch (error) {
+      setLoginError("Invalid Username of Password");
+      console.error("Error during Login", error);
+    }
+  }
   return (
     <div className="flex min-h-dvh flex-col items-center justify-center">
-      <Card className="mx-auto max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email"
+                  {...register("email")}
+                />
+                <p className="text-red-600">{errors.email?.message}</p>
               </div>
-              <Input id="password" type="password" required />
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  {...register("password")}
+                />
+                <p className="text-red-600">{errors.password?.message}</p>
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <p className="mt-4 text-center text-red-600">{loginError}</p>
             </div>
-            <Button type="submit" className="w-full">
-              Login
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link to="/signup" className="underline">
-              Sign up
-            </Link>
-          </div>
-        </CardContent>
-      </Card>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link to="/signup" className="underline">
+                Sign up
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </form>
     </div>
   );
 }
+
+// .matches(
+//   /^[a-zA-Z0-9._%+-]+@stud\.noroff\.no$/,
+//   "email must be a valid stud.noroff.no email address.",
+// )
